b0f68fea822cd112fccdb642ef4b242e3209b74e
Grant Kyle <sgrantkyle@gmail.com>
2020-03-19 18:01:38 -0500

    renamed burger.js file to correct a routing issue, modified server.js, modified handlebars files, added a button a text box to add a burger

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require("express");
 var PORT = process.env.PORT || 3002;
 
 var app = express();
-
+//simple lgger middleware
+app.use("*", (req, res, next) => {
+  console.log(req.url)
+  next()
+})
 // Serve static content for the app from the "public" directory in the application directory.
+
 app.use(express.static("public"));
 
 // Parse application body as JSON
@@ -23,7 +28,7 @@ var routes = require("./controllers/burgerController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
