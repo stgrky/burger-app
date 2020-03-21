578d8a9bec816320932a13afb517ddd04fa226e6
Grant Kyle <sgrantkyle@gmail.com>
2020-03-21 16:39:20 -0500

    issue correction with burger-block routing

--- config/orm.js
+++ config/orm.js
@@ -75,6 +75,7 @@ var orm = {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
+
     queryString += objToSql(objColVals);
     queryString += " WHERE ";
     queryString += condition;
--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,12 @@
-###
-Schema
+### Schema
 
 CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL
-	AUTO_INCREMENT,
-	name varchar
-	(255) NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
 	devour BOOLEAN DEFAULT false,
-	PRIMARY KEY
-	(id)
+	PRIMARY KEY (id)
 );
