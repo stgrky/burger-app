2dd617dbb0071eb2397f820a4e413fe0056cb141
Grant Kyle <sgrantkyle@gmail.com>
2020-03-21 16:59:18 -0500

    schema and seed changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,6 @@
-INSERT INTO burgers
-    (name)
-VALUES
-    ('Moo-moo Murderer');
-INSERT INTO burgers
-    (name)
-VALUES
-    ('Grease Gobbler');
-INSERT INTO burgers
-    (name, devour)
-VALUES
-    ('Fatfan', true);
-INSERT INTO burgers
-    (name, devour)
-VALUES
-    ('Carb Overload', true);
-INSERT INTO burgers
-    (name, devour)
-VALUES
-    ('Death Burger', true);
-INSERT INTO burgers
-    (name)
-VALUES
-    ('Plant-based Option');
+INSERT INTO burgers (name) VALUES ('Moo-Moo Murderer');
+INSERT INTO burgers (name) VALUES ('Waiting-to-die Whopper');
+INSERT INTO burgers (name, devour) VALUES ('Diabetes Deluxe', true);
+INSERT INTO burgers (name, devour) VALUES ('Grease Gobbler', true);
+INSERT INTO burgers (name, devour) VALUES ('Fat Führer', true);
+INSERT INTO burgers (name) VALUES ('Plant-based Burger');
